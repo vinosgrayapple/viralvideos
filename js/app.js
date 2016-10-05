78d7730d6c0d76b6b2ee2756bbdc70524de2fb52
Serhii Komarychev <komarichev@gmail.com>
2016-10-05 22:41:44 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@ function resetVideoHeight() {
 }
 
 function init() {
-    gapi.client.setApiKey("YOUR_PUBLIC_KEY");
+    gapi.client.setApiKey("AIzaSyDtDjgdi39C0x4DZIpb0GKJE7dUS9z8leY");
     gapi.client.load("youtube", "v3", function() {
         // yt api is ready
     });
